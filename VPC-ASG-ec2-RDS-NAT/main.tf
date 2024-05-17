e58a9bcf0a00cd5839b099c6ee17060ec6d677ca
Burhan <alpagut1993@gmail.com>
2024-05-17 16:33:34 +0200

    identifier

--- VPC-ASG-ec2-RDS-NAT/main.tf
+++ VPC-ASG-ec2-RDS-NAT/main.tf
@@ -180,7 +180,7 @@ resource "aws_db_instance" "main" {
   allocated_storage    = var.db_allocated_storage
   engine               = "mysql"
   instance_class       = var.db_instance_class
-  name                 = var.db_name
+  identifier           = var.db_name
   username             = var.db_user
   password             = var.db_password
   db_subnet_group_name = aws_db_subnet_group.main.name
